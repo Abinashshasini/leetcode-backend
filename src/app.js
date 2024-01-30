356dca33e3f8a9d0b37f920401658ba31b76622b
Abinash <abinashshasini125@gmail.com>
2024-01-31 00:17:43 +0530

    login added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(
@@ -15,6 +16,7 @@ app.use(
   })
 );
 app.use(express.static('public'));
+app.use(cookieParser());
 
 /** import routes */
 import userRouter from './routes/user.routes.js';
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,14 +3,33 @@ import { ApiError } from '../utils/apiError.js';
 import { ApiResponse } from '../utils/apiResponse.js';
 import { User } from '../models/user.model.js';
 
+/** Function to generate access and refresh token for user  */
+const handleGenerateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      'Oops! something went wrong while generating token'
+    );
+  }
+};
+
 /**
  * STEPS
- * 1. Get user details from frontend
- * 2. Validate fields -  not empty
- * 3. Check if user exists: email, username
- * 4. Create user object -  create entry in dB
- * 5. Remove password and refresh token filed from response
- * 6. Check user creation and return response
+ * 1. Get user details from frontend.
+ * 2. Validate fields -  not empty.
+ * 3. Check if user exists: email, username.
+ * 4. Create user object -  create entry in dB.
+ * 5. Remove password and refresh token filed from response.
+ * 6. Check user creation and return response.
  */
 const handleRegisterUser = asyncHandler(async (req, res) => {
   const { email, password, username } = req.body;
@@ -47,4 +66,39 @@ const handleRegisterUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, 'User registered successfully'));
 });
 
-export { handleRegisterUser };
+/*
+ * STEPS
+ * 1. Get user details from frontend.
+ * 2. Check if the username or email exists in the req.body.
+ * 3. Find user in the dB.
+ * 4. Match user password in the dB.
+ * 5. Generate access token and refresh token.
+ * 6. Remove password and refresh token from loggedin user (This can be solved by another dB call or updating the existing user)
+ * 6. Set secure cookies and user info to frontend.
+ */
+const handleLoginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    throw new ApiError(400, 'Email is required');
+  }
+
+  const user = await User.findOne(email);
+
+  if (!user) {
+    throw new ApiError(404, 'User does not exist');
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(404, 'Invalid user credentials');
+  }
+
+  const { accessToken, refreshToken } =
+    await handleGenerateAccessAndRefreshToken(user._id);
+
+  const loggedInUser = User.findOne(user._id).select('-password -refreshToken');
+});
+
+export { handleRegisterUser, handleLoginUser };
